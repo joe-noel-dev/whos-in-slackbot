04a5ce2b173c58511079e955f5c3a19f86231594
Joe Noël <joe.noel@focusrite.com>
2022-09-13 14:37:30 +0100

    Add requests for multiple users

--- source/index.ts
+++ source/index.ts
@@ -2,32 +2,32 @@ import {createMemoryDatabase} from './database';
 import {parseRequest} from './parser';
 import {processCommand} from './processor';
 
-const userId = 'U2147483697';
+const userIds = ['U2147483697', 'U11111111', 'U22222222', 'U33333333'];
 
 const requests = [
-  'default wfh',
-  'default ty',
-  'default hq',
-  'default Wycombe',
-  '21st sep tileyard',
-  '9th march hq',
-  '18th jan wfh',
-  '18th february tileyard',
-  '2022-10-14 hw',
-  'next friday HQ',
-  '<@U837468746|Buddy>',
-  '<@U837468734>',
-  'subscribe <@U837468746|Buddy>',
-  "who's in 8th October Tileyard",
-  "who's in tileyard ???",
+  {user: userIds[0], text: 'default wfh'},
+  {user: userIds[1], text: 'default ty'},
+  {user: userIds[2], text: 'default hq'},
+  {user: userIds[3], text: 'default Wycombe'},
+  {user: userIds[0], text: '21st sep tileyard'},
+  {user: userIds[1], text: '9th march hq'},
+  {user: userIds[2], text: '18th jan wfh'},
+  {user: userIds[3], text: '18th february tileyard'},
+  {user: userIds[0], text: '2022-10-14 hw'},
+  {user: userIds[1], text: 'next friday HQ'},
+  {user: userIds[2], text: '<@U837468746|Buddy>'},
+  {user: userIds[3], text: '<@U837468734>'},
+  {user: userIds[0], text: 'subscribe <@U837468746|Buddy>'},
+  {user: userIds[1], text: "who's in 8th October Tileyard"},
+  {user: userIds[2], text: "who's in tileyard ???"},
 ];
 
 const database = createMemoryDatabase();
 
 requests.forEach(async (request) => {
-  const command = parseRequest(request, userId);
+  const command = parseRequest(request.text, request.user);
   if (command) {
     const response = await processCommand(command, database);
-    console.log(`${request} => ${response}`);
+    console.log(`${request.text} => ${response}`);
   }
 });
